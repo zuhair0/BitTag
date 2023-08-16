89ca3324225e752c5ac4b49fd0a9896f4625a6a6
zuhair0 <117568070+zuhair0@users.noreply.github.com>
2023-08-16 02:30:33 -0700

    .

---
+++ BitTagUser/CutomerServices/CustomerWorkInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace BitTagUser.CutomerServices
+{
+    public class CustomerWorkInfo:ICustomerWorkInfo
+    {
+        private readonly HttpClient _httpClient;
+        public CustomerWorkInfo(HttpClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+        public async Task<List<CustomerWorkInfo>> GetCustomerWorkInfo()
+        {
+            return await _httpClient.GetFromJsonAsync<List<CustomerWorkInfo>>("api/controller/GetCustWorkInfo");
+        }
+        public async Task SaveCustomerWorkInfo(CustomerWorkInfo cwi)
+        {
+            await _httpClient.PostAsJsonAsync("api/controller/AddWorkInfo", cwi);
+        }
+    }
+}
---
+++ BitTagUser/CutomerServices/ICustomerWorkInfo.cs
@@ -0,0 +1,11 @@
+﻿using BitTagModels;
+
+namespace BitTagUser.CutomerServices
+{
+    public interface ICustomerWorkInfo
+    {
+        Task<List<CustomerWorkInfo>> GetCustomerWorkInfo();
+        Task SaveCustomerWorkInfo(CustomerWorkInfo cwi);
+        //Task DeleteCustomerWorkInfo(Guid id);
+    }
+}
