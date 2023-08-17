11d55c9d86811db22f35c558eaade2a92d48d174
Ayesha <ayeshadastagir50@gmail.com>
2023-08-17 00:23:56 -0700

    .

--- BitTagUser/Program.cs
+++ BitTagUser/Program.cs
@@ -12,16 +12,16 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddHttpClient<ICustomer, Customer>(
-    c => { c.BaseAddress = new Uri("https://localhost:7195"); }
+    c => { c.BaseAddress = new Uri("https://localhost:7125"); }
     );
 builder.Services.AddHttpClient<IVehicle, Vehicle>(
-    c => { c.BaseAddress = new Uri("https://localhost:7195"); }
+    c => { c.BaseAddress = new Uri("https://localhost:7125"); }
     );
 builder.Services.AddHttpClient<IOrgs, Orgs>(
-    c => { c.BaseAddress = new Uri("https://localhost:7195"); }
+    c => { c.BaseAddress = new Uri("https://localhost:7125"); }
     );
 builder.Services.AddHttpClient<ICustomerWorkInfo, CustomerWorkInfo>(
-    c => { c.BaseAddress = new Uri("https://localhost:7195"); }
+    c => { c.BaseAddress = new Uri("https://localhost:7125"); }
     );
 var app = builder.Build();
 
