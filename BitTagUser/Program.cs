ee7c63d48f06608051b73a3c4cb64ad8cb96cc65
Jahan <Jahan@DESKTOP-M9QE7L0>
2024-02-21 02:41:40 -0800

    authentication of user side done

--- BitTagUser/Program.cs
+++ BitTagUser/Program.cs
@@ -15,9 +15,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSyncfusionBlazor();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-builder.Services.AddScoped<UserAccountService>();
-builder.Services.AddScoped<ProtectedSessionStorage>();
+
 builder.Services.AddHttpClient<ICustomer, Customer>(
     c => { c.BaseAddress = new Uri("https://localhost:7195"); }
     );
@@ -36,6 +34,10 @@ builder.Services.AddHttpClient<IBitTagUsers, BitTaaguser>(
 //builder.Services.AddHttpClient<IBitTagUser, BitTagsUser>(
 //    c => { c.BaseAddress = new Uri("https://localhost:7195"); }
 //    );
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<UserAccountService>();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
